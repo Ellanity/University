3c6f0ab62a8812b60ae1bfdce44a558629025e15
Ellanity <topuser366@gmail.com>
2021-03-15 02:04:59 +0300

    Added course work cars

---
+++ Code for other students/Course Work Cars/source/MyForm.h
@@ -0,0 +1,186 @@
+#pragma once
+
+namespace CourseWork {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private: System::Windows::Forms::TextBox^ textBox1;
+	protected:
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::TextBox^ textBox3;
+
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(12, 29);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(205, 22);
+			this->textBox1->TabIndex = 0;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(223, 29);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(105, 22);
+			this->textBox2->TabIndex = 1;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(12, 9);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(75, 17);
+			this->label1->TabIndex = 2;
+			this->label1->Text = L"Car Name:";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(220, 9);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(108, 17);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Car Max Speed:";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(12, 57);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(316, 28);
+			this->button1->TabIndex = 4;
+			this->button1->Text = L"create simple car";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(12, 91);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(316, 28);
+			this->button2->TabIndex = 5;
+			this->button2->Text = L"create executive car";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(12, 125);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(316, 28);
+			this->button3->TabIndex = 6;
+			this->button3->Text = L"upgrade cars";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(12, 156);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(115, 17);
+			this->label3->TabIndex = 7;
+			this->label3->Text = L"Cars information:";
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(12, 176);
+			this->textBox3->Multiline = true;
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->ReadOnly = true;
+			this->textBox3->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
+			this->textBox3->ShortcutsEnabled = false;
+			this->textBox3->Size = System::Drawing::Size(316, 314);
+			this->textBox3->TabIndex = 8;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(342, 503);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->MaximizeBox = false;
+			this->MaximumSize = System::Drawing::Size(360, 550);
+			this->MinimumSize = System::Drawing::Size(360, 550);
+			this->Name = L"MyForm";
+			this->Text = L"Course Work";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+	#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e);
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e);
+	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e);
+
+	private: void showInformation();
+	};
+}
---
+++ TIIT-traditional-intelligent-information-technologies/Quadtree/in_one_file.cpp
@@ -0,0 +1,458 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+
+#define uns unsigned
+#define clout system("cls");
+#define clin  rewind(stdin);
+#define pause system("pause");
+
+
+using namespace std;
+
+const uns CHILD_COUNT = 4;
+const uns NEW_TABLE_CHILD_RC = int(sqrt(CHILD_COUNT) - 1);
+
+uns stringToUns(string str);
+
+struct Square
+{
+public:
+	uns value;
+	Square* child[CHILD_COUNT];
+	Square* parent;
+	bool have_child;
+};
+
+class Quadtree
+{
+private:
+	struct Square* root;
+	vector <vector <uns>> matrix;
+	uns tree_lvl;
+	uns max_tree_lvl;
+
+
+	void freeMemory(Square* square)
+	{
+		if (square != NULL)
+		{
+			for (uns i = 0; i < CHILD_COUNT; i++)
+				if (square->child[i] != NULL)
+					freeMemory(square->child[i]);
+			delete square;
+		}
+	}
+	void clearFields()
+	{
+		tree_lvl = 0;
+		max_tree_lvl = 0;
+		freeMemory(this->root);
+		root = new Square;
+		root = NULL;
+		for (int i = 0; i < matrix.size(); i++)
+			matrix[i].clear();
+		matrix.clear();
+	}
+
+	struct Square* createTree(Square* parent, vector <vector <uns>> table)
+	{
+
+		uns in_column = table.size();
+		uns in_row = table[0].size();
+
+		if (parent == NULL)
+		{
+			//Create childs for root
+			parent = new Square;
+			for (uns i = 0; i < CHILD_COUNT; i++)
+				parent->child[i] = NULL;
+
+			if (in_column > 1 && in_row > 1)
+			{
+				//R-row C-column RC-row or column
+				uns x = 0, y = 0, new_table_child_num_R = 0, new_table_child_num_C = 0;
+				uns new_table_elements_RC_quantity = in_column / 2;
+
+				for (uns i = 0; i < CHILD_COUNT; i++)
+				{
+					parent->child[i] = NULL;
+					vector <vector <uns>> new_table;
+
+					x = new_table_child_num_R * new_table_elements_RC_quantity;
+					y = new_table_child_num_C * new_table_elements_RC_quantity;
+
+
+					for (uns j = y; j < y + new_table_elements_RC_quantity; j++)
+					{
+						vector<uns> one_row;
+
+						for (uns k = x; k < x + new_table_elements_RC_quantity; k++)
+							one_row.push_back(table[j][k]);
+
+						new_table.push_back(one_row);
+					}
+
+					if (new_table_child_num_R == NEW_TABLE_CHILD_RC)
+					{
+						new_table_child_num_R = 0;
+						new_table_child_num_C++;
+					}
+					else
+						new_table_child_num_R++;
+
+					parent->child[i] = createTree(parent->child[i], new_table);
+					parent->have_child = true;
+					parent->value = 0;
+				}
+
+				//Make from same childs one node
+				if (parent != NULL)
+				{
+					bool common_bool = true;
+					uns common_value = parent->child[0]->value;
+
+					for (uns i = 0; i < CHILD_COUNT; i++)
+						if (parent->child[i]->value != common_value || parent->child[i]->have_child == true)
+							common_bool = false;
+
+
+					if (common_bool)
+					{
+						for (uns i = 0; i < CHILD_COUNT; i++)
+						{
+							freeMemory(parent->child[i]);
+							parent->child[i] = NULL;
+						}
+
+						parent = NULL;
+						parent = new Square;
+
+						parent->value = common_value;
+						parent->have_child = false;
+						for (uns i = 0; i < CHILD_COUNT; i++)
+							parent->child[i] = NULL;
+					}
+				}
+			}
+			else
+			{
+				parent->value = table[0][0];
+				parent->have_child = false;
+				for (uns i = 0; i < CHILD_COUNT; i++)
+					parent->child[i] = NULL;
+			}
+		}
+		return parent;
+	}
+	void printTree(Square* square)
+	{
+		if (square != NULL)
+		{
+			tree_lvl++;
+			if (tree_lvl >= max_tree_lvl)
+				max_tree_lvl = tree_lvl;
+
+			cout << "       ";
+			if (!square->have_child)
+			{
+				for (uns i = 0; i < tree_lvl - 1; i++)
+					cout << "    ";
+				cout << "|___" << square->value << "\n";
+			}
+
+			else
+			{
+				for (uns i = 0; i < tree_lvl - 1; i++)
+					cout << "    ";
+				cout << "|___" << "\n";
+
+				for (uns i = 0; i < CHILD_COUNT; i++)
+				{
+
+					printTree((square->child[i]));
+				}
+			}
+			tree_lvl--;
+		}
+	}
+	struct Square* readTree(Square* parent, string action)
+	{
+		if (parent == NULL)
+		{
+			parent = new Square;
+			for (uns i = 0; i < CHILD_COUNT; i++)
+				parent->child[i] = NULL;
+			parent->value = 0;
+			parent->have_child = false;
+		}
+
+		if (action == "-")
+		{
+			string next;
+			for (uns i = 0; i < CHILD_COUNT; i++)
+			{
+				if (parent->child[i] == NULL)
+				{
+					cin >> next;
+					parent->child[i] = readTree(parent->child[i], next);
+				}
+			}
+			parent->have_child = true;
+		}
+
+		else
+		{
+			uns num = stringToUns(action);
+			parent->value = num;
+		}
+
+		return parent;
+	}
+
+	struct Square* clearTree(Square* parent)
+	{
+		if (parent->have_child)
+		{
+			bool common_bool = true;
+			uns common_value = parent->child[0]->value;
+
+			for (uns i = 0; i < CHILD_COUNT; i++)
+				if (parent->child[i]->value != common_value || parent->child[i]->have_child == true)
+					common_bool = false;
+
+			if (common_bool)
+			{
+				freeMemory(parent);
+
+				parent = new Square;
+				cout << common_value << "\n";
+				parent->value = common_value;
+
+				parent->have_child = false;
+				for (uns i = 0; i < CHILD_COUNT; i++)
+					parent->child[i] = NULL;
+			}
+
+			if (parent->have_child)
+			{
+				for (uns i = 0; i < CHILD_COUNT; i++)
+					parent->child[i] = clearTree(parent->child[i]);
+			}
+			parent = clearTree(parent);
+		}
+		return parent;
+	}
+
+	vector <vector <uns>> createMatrix(Square* parent, vector <vector <uns> > matrix, uns child_num, uns last_i, uns last_j)
+	{
+		int lvl = (max_tree_lvl - tree_lvl - 1);
+		if (lvl < 0) lvl = 0;
+
+		uns elems_quantity = lvl * (CHILD_COUNT / 2);
+		if (elems_quantity <= 0)
+			elems_quantity = 1;
+
+		uns local_i = uns(child_num / (CHILD_COUNT / 2) * elems_quantity);
+		uns local_j = (child_num % (CHILD_COUNT / 2)) * elems_quantity;
+
+		int i_start = local_i + last_i;
+		int j_start = local_j + last_j;
+
+		if (parent->have_child == false)
+		{
+			for (uns i = i_start; i < i_start + elems_quantity; i++)
+			{
+				for (uns j = j_start; j < j_start + elems_quantity; j++)
+				{
+					matrix[i][j] = parent->value;
+				}
+			}
+		}
+		else
+		{
+			tree_lvl++;
+			for (uns i = 0; i < CHILD_COUNT; i++)
+				matrix = createMatrix(parent->child[i], matrix, i, i_start, j_start);
+			tree_lvl--;
+		}
+		return matrix;
+	}
+	void printTable(vector <vector <uns>> matrix)
+	{
+		uns n = matrix.size();
+		for (uns i = 0; i < n; i++)
+		{
+			for (uns j = 0; j < n; j++)
+			{
+				cout << matrix[i][j] << " ";
+			}
+			cout << "\n";
+		}
+	}
+	vector <vector <uns>> readMatrix(uns n)
+		{
+			uns elem;
+			string elem_str;
+			vector <vector <uns>> matrix;
+			for (uns i = 0; i < n; i++)
+			{
+				vector <uns> row;
+				for (uns j = 0; j < n; j++)
+				{
+					cin >> elem_str;
+					elem = stringToUns(elem_str);
+					row.push_back(elem);
+				}
+				matrix.push_back(row);
+			}
+			return matrix;
+		}
+
+public:
+	Quadtree()
+	{
+		this->root = NULL;
+		this->tree_lvl = 0;
+		this->max_tree_lvl = 0;
+	}
+
+	~Quadtree()
+	{
+		this->freeMemory(this->root);
+	}
+
+	void buildTree()
+	{
+		cout << "Quadtree its a structure, that consist of squares\n" <<
+				"Big Square must be N*N elements, where N is a power of two, enter N:\n";
+
+		string N;
+		cin >> N; clin;
+		uns n = stringToUns(N);
+
+		if (((n & (n - 1)) == 0) && n > 0)
+		{
+			this->clearFields();
+
+			cout << "Its cool!\nNow enter matrix N*N:\n";
+			this->matrix = this->readMatrix(n);
+			clin;
+
+			cout << "The matrix that could be read:\n";
+			this->printTable(this->matrix);
+
+			this->root = NULL;
+			this->root = this->createTree(this->root, this->matrix);
+
+			cout << "\nTree construction completed successfully\n\nroot___\n";
+			this->printTree(this->root);
+		}
+		else
+			cout << "It is NOT a power of two, try once more.\n";
+	}
+
+	void buildMatrix()
+	{
+		this->clearFields();
+
+		cout << "Now enter new \"Quadtree\":\n" <<
+			"1) symbol '-' will create new node with " << CHILD_COUNT << " childs\n" <<
+			"then you need enter this childs\n" <<
+			"if you want to enter number, just do it\n" <<
+			"if you want creare in node new node read 1\n";
+
+		string action;
+		cin >> action;
+
+		this->root = this->readTree(this->root, action);
+		this->root = this->clearTree(this->root);
+
+		cout << "\nTree that could be read:\nroot___\n";		
+		this->printTree(this->root);
+
+		uns size = pow(2, this->max_tree_lvl - 1);
+		if (size < 1) size = 1;
+		for (uns i = 0; i < size; i++)
+		{
+			vector <uns> row;
+			for (uns i = 0; i < size; i++)
+				row.push_back(0);
+
+			this->matrix.push_back(row);
+		}
+
+		//cout << "\nMatrix sizes: " << this->matrix.size() << " " << this->matrix[0].size() << "\n";
+
+		this->matrix = this->createMatrix(this->root, this->matrix, 0, 0, 0);
+		this->printTable(this->matrix);
+	}
+
+	struct Square* getRoot()
+	{
+		return this->root;
+	};
+	vector <vector <uns>> getMatrix()
+	{
+		return this->matrix;
+	}
+
+	void printRoot()
+	{
+		cout << "\nroot___\n";
+		this->printTree(this->root);
+	};
+	void printMatrix()
+	{
+		this->printTable(this->matrix);
+	}
+};
+
+int main()
+{
+	Quadtree quadtree;
+	while (true)
+	{
+		clout;
+		cout << "1) Build new tree from matrix\n" <<
+			"2) Build new matrix from tree\n" <<
+			"3) Print tree\n" <<
+			"4) Print matrix\n" <<
+			"5) Exit\n";
+
+		string choice;
+		cin >> choice;
+
+		if (choice == "1")
+			quadtree.buildTree();
+		else if (choice == "2")
+			quadtree.buildMatrix();
+		else if (choice == "3")
+			quadtree.printRoot();
+		else if (choice == "4")
+			quadtree.printMatrix();
+		else if (choice == "5")
+			return 0;
+		else
+			cout << "No such command\n";
+		pause;
+	}
+}
+
+uns stringToUns(string str)
+{
+	uns ans = 0;
+	for (uns i = 0; i < str.size(); i++)
+	{
+		ans *= 10;
+		if (str[i] - '0' >= 0 && str[i] - '0' < 10)
+			ans += str[i] - '0';
+		else if (str[i] - '0' > 50)
+			ans -= 38;
+		else
+			ans /= 10;
+		if (i == 0 && str[i] == '-')
+			ans = 0;
+	}
+	return ans;
+}
