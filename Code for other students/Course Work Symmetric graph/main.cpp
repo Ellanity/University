de4f4366c07322bc09f1d4438e05d1afce9e2cd3
Ellanity <topuser366@gmail.com>
2021-05-10 00:32:20 +0300

    Update main.cpp
    > In coursework symmetric graph

--- Code for other students/Course Work Symmetric graph/main.cpp
+++ Code for other students/Course Work Symmetric graph/main.cpp
@@ -9,11 +9,15 @@ int main() {
 	int n, m;
 	cout << "Количество вершин: ";	cin >> n;
 	cout << "Количество ребер: ";	cin >> m;
+	// cout << "Nodes quantity: ";	cin >> n;
+	// cout << "Edges quantity: ";	cin >> m;
 
 	// Заполнение двумерного вектора нулями
+	// Filling a two-dimensional vector with zeros
 	vector <vector <int>> graph(m, vector <int>(n, 0));
 
 	// Заполнение матрицы инцидентности
+	// Filling the incidence matrix
 	int a, b;
 	for (int i = 0; i < m; i++) {
 		cin >> a >> b;
@@ -25,52 +29,77 @@ int main() {
 	}
 
 	// Вывод матрицы инцидентности
+	// Output of the incidence matrix
 	/*for (int i = 0; i < m; i++) {
 		for (int j = 0; j < n; j++)
 			cout << graph[i][j] << " ";
 		cout << "\n";
 	}*/
 
-	// Вершины[x], к которым существуют ребра из всех вершин[y],
-	// К которым существуют ребра из вершин[x]
-	vector<int> bfn;
-	
-	for (int i = 0; i < n; i++) { // Из каждой вершины цикл проверки
-		//vector<int> fn; // Вершины к которым есть ребра из вершины i
-
-		bool bfnb = true; // Подтверждение, что из всех вершин, 
-						 // к которым есть ребра из i, есть обратные ребра 
-		for (int j = 0; j < m; j++) { // Для каждого ребра мы проверяем
-			if (graph[j][i] == 1) { // Если оно ведет из вершины i 
-				for (int k = 0; k < n; k++) { // Находим в какую именно вершину k ведет
-					if (graph[j][k] == 2) { // Когда находим, проверяем есть ли ребро из k в вершину i
-						for (int h = 0; h < m; h++) { // Для каждого ребра смотрим 
-							if (graph[h][k] == 1 && graph[h][i] == 2) {// Есть ли ребро из k, ведет ли оно в i
-								bfnb = true;
-								break;
-							}
-							bfnb = false;
-						}
-					}
-					if (!bfnb)
-						break;
-				}
-			}
-			if (!bfnb)
-				break;
+	vector <int> symmetric_edges;
+	for (int i = 0; i < m; i++) {
+		int p1 = 0, p2 = 0;
+		for (int j = 0; j < n; j++) {
+			if (graph[i][j] == 1)
+				p1 = j;
+			if (graph[i][j] == 2)
+				p2 = j;
 		}
-		
-		// Если подтверждение сохранено, запоминаем вершину
-		if (bfnb)
-			bfn.push_back(i);
+		if (p1 != p2)
+			for (int j = 0; j < m; j++)
+				if (graph[j][p1] == 2 && graph[j][p2] == 1) {
+					symmetric_edges.push_back(i);
+					//cout << j << " " << p1 << " " << p2 << "\n";
+					break;
+				}
 	}
 
-	if (bfn.size() == 0)
-		cout << "Граф антисимметричный";
-	else if (bfn.size() != n)
-		cout << "Граф частично симметричный";
-	else if (bfn.size() == n)
-		cout << "Граф симметричный";
+	/*for (int i = 0; i < symmetric_edges.size(); i++)
+		cout << symmetric_edges[i] + 1 << " ";
+	cout << "\n";*/
+
+	int nse = symmetric_edges.size();
+	if (nse == 0)
+		cout << "Граф антисимметричный\n";
+		// cout << "Antisymmetric graph\n";
+	else if (nse != m)
+		cout << "Граф частично симметричный\n";
+		// cout << "Partially symmetric graph\n";
+	else if (nse == m)
+		cout << "Граф симметричный\n";
+		// cout << "Symmetric graph\n";
 
 	return 0;
 }
+
+/******************|
+_______TESTS_______|
+				   |
+_PARTLY__SYMMETRIC_|
+5 5
+1 2
+2 3
+4 3
+5 4
+3 4 
+				   |
+_____SYMMETRIC_____|
+4 8
+1 2
+2 3
+3 4
+4 1
+1 4
+4 3
+3 2
+2 1
+				   |
+___ANTISYMMETRIC___|
+3 2
+1 2
+3 2
+				   |
+_____LITERATURE____|
+https://habr.com/ru/post/519998/
+http://khpi-iip.mipk.kharkiv.edu/library/datastr/book_sod/kgsu/din_0087.html
+*******************/
